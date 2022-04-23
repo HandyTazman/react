3b09b5108500a4742b948a0ccc6c785cecadec61
Sam Nanayakkara <78934646+HandyTazman@users.noreply.github.com>
2022-04-23 09:04:50 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# @metaplex/react
+
+Metaplex React SDK
+
+> **In Development** - All interfaces are very likely to change very frequently. Please be aware.
+
+- [x] Metaplex hook
+- [x] Wallets
+- [ ] More hooks
+- [ ] GraphQL client
+- [ ] GraphQL caching
---
+++ release.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'refactor', release: 'patch' },
+          { type: 'style', release: 'patch' },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message:
+          'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
